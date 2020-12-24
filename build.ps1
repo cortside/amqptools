64cde962bd137ead28a627e07186fa79bb149593
Cort Schaefer <cort@eschaefer.net>
2020-12-23 18:33:46 -0700

    update build for alpine

--- build.ps1
+++ build.ps1
@@ -9,9 +9,9 @@ echo $env:Configuration
 echo $runtime
 
 & ./clean.ps1
-dotnet build -r $runtime src/AmqpShovel/AmqpShovel.csproj --configuration $env:Configuration /property:Version=$env:APPVEYOR_BUILD_VERSION
-dotnet build -r $runtime src/AmqpPublisher/AmqpPublisher.csproj --configuration $env:Configuration /property:Version=$env:APPVEYOR_BUILD_VERSION
-dotnet build -r $runtime src/AmqpQueue/AmqpQueue.csproj --configuration $env:Configuration /property:Version=$env:APPVEYOR_BUILD_VERSION
-dotnet publish -r $runtime -c Debug /p:PublishSingleFile=true --output publish/$runtime src/AmqpShovel/AmqpShovel.csproj
-dotnet publish -r $runtime -c Debug /p:PublishSingleFile=true --output publish/$runtime src/AmqpPublisher/AmqpPublisher.csproj
-dotnet publish -r $runtime -c Debug /p:PublishSingleFile=true --output publish/$runtime src/AmqpQueue/AmqpQueue.csproj
+#dotnet build -r $runtime src/AmqpShovel/AmqpShovel.csproj --configuration $env:Configuration /property:Version=$env:APPVEYOR_BUILD_VERSION
+#dotnet build -r $runtime src/AmqpPublisher/AmqpPublisher.csproj --configuration $env:Configuration /property:Version=$env:APPVEYOR_BUILD_VERSION
+#dotnet build -r $runtime src/AmqpQueue/AmqpQueue.csproj --configuration $env:Configuration /property:Version=$env:APPVEYOR_BUILD_VERSION
+dotnet publish -r $runtime -c Debug /p:PublishSingleFile=true /p:PublishTrimmed=true --output publish/$runtime src/AmqpShovel/AmqpShovel.csproj
+dotnet publish -r $runtime -c Debug /p:PublishSingleFile=true /p:PublishTrimmed=true --output publish/$runtime src/AmqpPublisher/AmqpPublisher.csproj
+dotnet publish -r $runtime -c Debug /p:PublishSingleFile=true /p:PublishTrimmed=true --output publish/$runtime src/AmqpQueue/AmqpQueue.csproj
