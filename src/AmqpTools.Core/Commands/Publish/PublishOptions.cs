113caf91b96d1113c8fa236258169e1ee3d8622a
Cort Schaefer <cort@eschaefer.net>
2025-03-20 09:55:38 -0600

    add back ignored publish command and add dotnet tool metadata to project

---
+++ src/AmqpTools.Core/Commands/Publish/PublishOptions.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics.CodeAnalysis;
+using CommandLine;
+
+namespace AmqpTools.Core.Commands.Publish {
+    [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicMethods)]
+    [Verb("publish", HelpText = "publishes an amqp message")]
+    public class PublishOptions : BaseOptions {
+        public PublishOptions() { }
+
+        [Option("data", Required = false, HelpText = "Message data/json")]
+        public string Data { get; set; }
+        [Option("file", Required = false, HelpText = "filename for Message data/json")]
+        public string File { get; set; }
+        [Option("eventtype", Required = true, HelpText = "Event type (event class full name)")]
+        public string EventType { get; set; }
+    }
+}
